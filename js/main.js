bb17c5341dcba9b01c5d86c9ebb712ab6c42f275
Ryohei <ryogame11@gmail.com>
2021-03-17 00:08:21 +0900

    add task

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 var app = new Vue({
-	// options
   el: '#app',
-  data: {
-  	message: 'Hello world!!'
+  data:{
+    newItem:"",
+    todos:[]
+  },
+  methods:{
+    addItem:function(e){
+      var todo = {
+        item: this.newItem
+      };
+      this.todos.push(todo);
+    }
   }
 })
